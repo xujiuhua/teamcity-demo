f66a3068f36510cf378335dda62d22893f62e12b
jiuhua.xu <xujiuhuamoney@163.com>
2019-03-28 13:34:00 +0800

    Initial commit

---
+++ src/main/java/com/xujh/teamcity/TeamcityDemoApplication.java
@@ -0,0 +1,23 @@
+package com.xujh.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TeamcityDemoApplication {
+
+    @GetMapping("test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("hello teamcity");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityDemoApplication.class, args);
+    }
+
+
+}
